3f15624ea3b3ac9098812645ed6561ac86e4cc13
dlsf <munita.gavai@gmail.com>
2021-07-09 21:42:05 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# My Clojure Adventures
+Some small programs I wrote on my journey.<p>
+Will be expanded and improved as I advance. Not recommended as a learning resource because I'm just a beginner.
---
+++ src/leetspeak.clj
@@ -0,0 +1,18 @@
+(ns leetspeak
+  (:require [clojure.string :as str]))
+
+(defn text-to-leetspeak [text]
+  (let [chars {"i" 1
+               "l" 1
+               "e" 3
+               "a" 4
+               "s" 5
+               "t" 7
+               "b" 8
+               "g" 9
+               "o" 0}]
+    (->> text (map #(get chars (str/lower-case %) %)) (str/join ""))))
+
+(defn -main []
+  (println "Please enter the text:")
+  (println (text-to-leetspeak (read-line))))
